d65ac55fb91258b747fb749fcdc8f74139acbdb2
xujian03 <xujian03@baijiahulian.com>
2020-04-28 10:50:34 +0800

    增加修改pom代码

--- src/main/java/SwaggerGenDeploy.java
+++ src/main/java/SwaggerGenDeploy.java
@@ -1,6 +1,18 @@
 import lombok.extern.slf4j.Slf4j;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 import org.yaml.snakeyaml.Yaml;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.transform.Result;
+import javax.xml.transform.Source;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
 import java.io.*;
 import java.util.HashMap;
 import java.util.Map;
@@ -41,12 +53,51 @@ public class SwaggerGenDeploy {
                 log.info(s1);
             }
             process1.waitFor();
+
+            /*
+            修改pom文件，plugin标签增加以下节点
+            <configuration>
+                    <includes>
+                        <include>
+                            <groupId>nothing</groupId>
+                            <artifactId>nothing</artifactId>
+                        </include>
+                    </includes>
+                    <skip>true</skip>
+                </configuration>
+             */
+            File file = new File(tmpDir,"pom.xml");
+            DocumentBuilder newDocumentBuilder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
+            Document doc = newDocumentBuilder.parse(file);
+            Element root = doc.getDocumentElement();
+            NodeList nodeList = root.getElementsByTagName("plugin");
+            Node plugin = nodeList.item(0);
+            Element configuration = doc.createElement("configuration");
+            Element includes = doc.createElement("includes");
+            Element include = doc.createElement("include");
+            Element groupIdTag = doc.createElement("groupId");
+            groupIdTag.setTextContent("nothing");
+            Element artifactIdTag = doc.createElement("artifactId");
+            artifactIdTag.setTextContent("nothing");
+            include.appendChild(groupIdTag);
+            include.appendChild(artifactIdTag);
+            includes.appendChild(include);
+            configuration.appendChild(includes);
+            Element skip = doc.createElement("skip");
+            skip.setTextContent("true");
+            configuration.appendChild(skip);
+            plugin.appendChild(configuration);
+            Transformer transformer = TransformerFactory.newInstance().newTransformer();
+            Source source = new DOMSource(doc);
+            Result result = new StreamResult(file);
+            transformer.transform(source, result);
+
             log.info("正在发布代码...");
             //仓库地址
             String repoUrl = args[1];
             //仓库id
             String repoId = args[2];
-            String command2 = "mvn deploy:deploy-file -DgroupId="+groupId+" -DartifactId="+artifactId+" -Dversion="+version+" -Dfile=/tmp/swagger-tmp/"+artifactId+"/pom.xml -Dpackaging=jar -Durl="+repoUrl+" -DrepositoryId="+repoId+" -DskipTests";
+            String command2 = "mvn deploy:deploy-file -DgroupId="+groupId+" -DartifactId="+artifactId+" -Dversion="+version+" -Dfile=/tmp/swagger-tmp/"+artifactId+"/pom.xml -Dpackaging=pom -Durl="+repoUrl+" -DrepositoryId="+repoId+" -DskipTests";
             Process process2 = Runtime.getRuntime().exec(command2);
             String s2;
             BufferedReader reader2 = new BufferedReader(new InputStreamReader(process2.getInputStream()));
